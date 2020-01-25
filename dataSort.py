a0e0b08d8e3e7b5a4a73b067a50bacbba7980303
Hunter McCraw <hunterlovescoffee@gmail.com>
2020-01-25 04:10:10 -0700

    Timestamping // Periodic task test
    Co-authored-by: robm1158 <mullins097956@gmail.com>

--- dataSort.py
+++ dataSort.py
@@ -1,2 +1,15 @@
+import threading
+import time
+
 print("intial Push for dev branch")
 print("Hunter initial push")
+# Phase 1 - Commence
+
+def timePrintingExample():
+    print ("ctime : ", time.ctime())
+    
+WAIT_TIME_SECONDS = 5
+
+ticker = threading.Event()
+while not ticker.wait(WAIT_TIME_SECONDS):
+    timePrintingExample()
