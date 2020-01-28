bc95779ab1d6ca558abf4882f6ba028743db78e3
Rob Mullins <rob.d.mullins@lmco.com>
2020-01-28 11:08:50 -0700

    cleaning up useless code, added sudo-code

--- filterTools.py
+++ filterTools.py
@@ -18,38 +18,40 @@ import sqlUpdater
 # for /market/{region_id}/orders/
 # Ex) https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=buy&page=1&type_id=34
 
-def sortData(conn,tableName,url):
+def sortData(sql,tableName,url):
 
     response = requests.get(url)
-
     data = {}
-
     for index in response.json():
         id = index['order_id']
-        print("Inputting: " + str(id))
-        data[id] = []
-        data[id].append({
-            'duration':index['duration'],
-            'is_buy_order':index['is_buy_order'],
-            'issued':index['issued'],
-            'location_id':index['location_id'],
-            'min_volume':index['min_volume'],
-            'price':index['price'],
-            'range':index['range'],
-            'system_id':index['system_id'],
-            'type_id':index['type_id'],
-            'volume_remain':index['volume_remain'],
-            'volume_total':index['volume_total']
-            })
-        
         values = (index['order_id'],index['is_buy_order'], index['issued'],index['location_id'],index['min_volume'],
                     index['price'],index['range'],index['system_id'],index['type_id'],
                     index['volume_remain'],index['volume_total'])
-        conn.sqlLoadData(values, tableName)
+        sql.sqlLoadData(values, tableName)
 
-    file = open("D:\\Code\\EveFinance\\buyOrders.json","w")
-    json.dump(data,file)
     return("Finished Sorting Buys")
+
+def rowLookUp(sql,rowData,filterTable):
+    counter = 0
+    sql.crsr.execute("SELECT * FROM "+filterTable)
+    archives = sql.crsr.fetchall()
+    
+    if len(archives) == 0 :
+       # values = (index['order_id'],index['is_buy_order'], index['issued'],index['location_id'],index['min_volume'],
+       #             index['price'],index['range'],index['system_id'],index['type_id'],
+       #             index['volume_remain'],index['volume_total'])
+       # conn.sqlLoadData(values, tableName)
+
+    for index in range(len(archives)):
+        data = sql.sqlReadData(index + 1, filterTable)
+        #if row (data[0] != rowData[0]) || (data[1] != rowData[1])
+        # set values like above
+        #sql.sqlLoadData()
+        #counter = last index value
+        #else:
+        #log nothing added
+        #counter = index
+
 
 def filterSqlData(sql,table):
     # create table
